cc45f00024e84ab733f1a8f5244e6ba4a09bd24b
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-07 22:01:43 -0600

    Now isDirectory only returns true if the path exists

--- webserver/server.fsx
+++ webserver/server.fsx
@@ -10,26 +10,29 @@ open Suave.Filters
 open Suave.Operators
 
 let isDirectory (path : string) = 
+  if File.Exists path then
     let attributes = File.GetAttributes(path)
     attributes.HasFlag(FileAttributes.Directory)
+  else 
+    false
 
 let resolvePath (rootPath : string) (fileName : string) =
-    let fileName =
-      if Path.DirectorySeparatorChar.Equals('/') then fileName
-      else fileName.Replace('/', Path.DirectorySeparatorChar)
-    let calculatedPath =
-      Path.Combine(rootPath, fileName.TrimStart([| Path.DirectorySeparatorChar; Path.AltDirectorySeparatorChar |]))
-      |> Path.GetFullPath
-      // If the path resolves to a directory, then use the index.html file in that directory.
-      |> fun p -> if isDirectory p then Path.Combine(p, "index.html") else p
-    printfn "%s" calculatedPath
-    if calculatedPath.StartsWith rootPath then
-      calculatedPath
-    else raise <| Exception("File canonalization issue.")
+  let fileName =
+    if Path.DirectorySeparatorChar.Equals('/') then fileName
+    else fileName.Replace('/', Path.DirectorySeparatorChar)
+  let calculatedPath =
+    Path.Combine(rootPath, fileName.TrimStart([| Path.DirectorySeparatorChar; Path.AltDirectorySeparatorChar |]))
+    |> Path.GetFullPath
+    // If the path resolves to a directory, then use the index.html file in that directory.
+    |> fun p -> if isDirectory p then Path.Combine(p, "index.html") else p
+  printfn "%s" calculatedPath
+  if calculatedPath.StartsWith rootPath then
+    calculatedPath
+  else raise <| Exception("File canonalization issue.")
 
 let webBrowse rootPath : WebPart =
-    warbler (fun ctx ->
-      Files.file (resolvePath rootPath ctx.request.path))
+  warbler (fun ctx ->
+    Files.file (resolvePath rootPath ctx.request.path))
 
 let app : WebPart =
   choose [
