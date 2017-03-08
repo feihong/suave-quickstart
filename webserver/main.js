4a53c8de9a39a494218af5d54525b30654345bf9
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-07 21:53:46 -0600

    Add web server example

---
+++ webserver/main.js
@@ -0,0 +1,2 @@
+let num = Math.floor(Math.random() * 100)
+document.querySelector('.number').textContent = `Today's lucky number is ${num}!`
---
+++ webserver/server.fsx
@@ -0,0 +1,40 @@
+(*
+Note that Files.browse is defined in Suave/Combinators.fs
+
+*)
+#r "../packages/Suave/lib/net40/Suave.dll"
+open System
+open System.IO
+open Suave
+open Suave.Filters
+open Suave.Operators
+
+let isDirectory (path : string) = 
+    let attributes = File.GetAttributes(path)
+    attributes.HasFlag(FileAttributes.Directory)
+
+let resolvePath (rootPath : string) (fileName : string) =
+    let fileName =
+      if Path.DirectorySeparatorChar.Equals('/') then fileName
+      else fileName.Replace('/', Path.DirectorySeparatorChar)
+    let calculatedPath =
+      Path.Combine(rootPath, fileName.TrimStart([| Path.DirectorySeparatorChar; Path.AltDirectorySeparatorChar |]))
+      |> Path.GetFullPath
+      // If the path resolves to a directory, then use the index.html file in that directory.
+      |> fun p -> if isDirectory p then Path.Combine(p, "index.html") else p
+    printfn "%s" calculatedPath
+    if calculatedPath.StartsWith rootPath then
+      calculatedPath
+    else raise <| Exception("File canonalization issue.")
+
+let webBrowse rootPath : WebPart =
+    warbler (fun ctx ->
+      Files.file (resolvePath rootPath ctx.request.path))
+
+let app : WebPart =
+  choose [
+    GET >=> webBrowse __SOURCE_DIRECTORY__
+    RequestErrors.NOT_FOUND "Page not found." 
+  ]
+
+startWebServer defaultConfig app
